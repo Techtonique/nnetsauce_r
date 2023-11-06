21b83a45bb54aa41f952d2d5e26cc4ebeda416f0
Thierry Moudiki <thierry.moudiki@gmail.com>
2023-11-06 10:48:31 +0100

    install scikit-learn

--- R/zzz.R
+++ R/zzz.R
@@ -10,6 +10,14 @@ sklearn <- NULL
   # try(reticulate::use_virtualenv('./r-reticulate'),
   #     silent = TRUE)
   try(reticulate::py_install(
+    "scikit-learn",
+    #envname = "r-reticulate",
+    pip = TRUE,
+    pip_options = "--upgrade",
+    pip_ignore_installed = TRUE
+  ),
+  silent = TRUE)
+  try(reticulate::py_install(
     "nnetsauce",
     #envname = "r-reticulate",
     pip = TRUE,
